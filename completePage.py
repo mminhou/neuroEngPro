730a75446351b65e35a2772fc30caaf886028c68
R _ R <exit19093@gmail.com>
2021-06-02 12:52:35 +0900

    update: complete page

--- completePage.py
+++ completePage.py
@@ -1,17 +1,30 @@
 from module import *
+import math
 
 class CompletePage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-
+        self.time = 0
 
         # Wallpaper
         self.canvas = tk.Canvas(self, width=800, height=600, bg='white')
         self.canvas.pack(fill="both", expand=True)
 
-        # Graph Title
-        self.canvas.create_text(350, 100, text="Clear!", font=("Helvetica", 70, 'bold'))
+        # Title
+        self.canvas.create_text(700, 200, text="Clear!", font=("Helvetica", 80, 'bold'), fill="Red")
+
+        # 플레이 시간 계산
+        if self.time / 60 >= 1:
+            self.play_time = "플레이 시간 : " + str(math.trunc(self.time / 60)) + "분" + str(self.time % 60) + "초"
+        else:
+            self.play_time = "플레이 시간 : " + str(self.time) + "초"
+
+        # 플레이 시간 출력
+        self.canvas.create_text(700, 400, text=self.play_time, font=('Helvetica', 70, "bold"))
+
+        # Graph Image
+        # self.image = self.canvas.create_image(740, 500, image=self.graphWall)
 
         # Graph Image
         # self.image = self.canvas.create_image(740, 500, image=self.graphWall)
