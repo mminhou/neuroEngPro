e3e5aced10f9bc8d6ca181a7ac21cd805f89e224
R _ R <exit19093@gmail.com>
2021-06-08 14:26:32 +0900

    update: commentart completePage

--- completePage.py
+++ completePage.py
@@ -2,23 +2,24 @@ from module import *
 import math
 
 class CompletePage(tk.Frame):
+    ''' CompletePage '''
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.time = 0
 
-        # Wallpaper
+        ''' Complete Wallpaper '''
         self.canvas = tk.Canvas(self, width=800, height=600, bg='white')
         self.canvas.pack(fill="both", expand=True)
 
-        # Title
+        ''' Complete Title '''
         self.canvas.create_text(700, 200, text="Clear!", font=("Helvetica", 80, 'bold'), fill="Red")
 
-        # Clear Text
-        self.canvas.create_text(700, 300, text="Play Time", font=("Helvetica", 70, 'bold'), fill='Green')
+        ''' Show paly Time txt  '''
+        self.canvas.create_text(700, 300, text="Play Time >> press 'a' key", font=("Helvetica", 70, 'bold'), fill='Green')
         self.canvas.bind('<a>', lambda _: self.read_txt())
 
-        # Go Home Button
+        '''  Go Home(Intro Button) Button '''
         homeButton = tk.Button(self, text="go home",
                                font=("Helvetica", 80, 'bold'),
                                borderwidth=0, highlightthickness=0,
@@ -26,6 +27,7 @@ class CompletePage(tk.Frame):
         self.canvas.create_window(700, 500, window=homeButton)
 
     def read_txt(self):
+        ''' read play_time.txt '''
         self.f = open('factory/image/play_time.txt', 'r+t')
         self.a = self.f.readline()
         self.b = (int)(self.a)
