a66849f4c8ae67a33ca0507c2065dddb9f3acadd
R _ R <exit19093@gmail.com>
2021-06-08 14:37:19 +0900

    update: modules

--- module.py
+++ module.py
@@ -1,3 +1,4 @@
+''' modules '''
 import tkinter as tk
 from tkinter import font as tkfont
 from PIL import Image, ImageTk, ImageSequence
@@ -5,4 +6,5 @@ import time
 import matplotlib as plt
 from tkinter import filedialog
 from datetime import datetime
+import math
 
