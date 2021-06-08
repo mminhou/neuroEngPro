e4ec6b8a1bf7f3001d6fc0f84d0785e8db6c08d7
R _ R <exit19093@gmail.com>
2021-06-08 16:00:37 +0900

    update: commentary process

--- processing.py
+++ processing.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-import time
 
-# Biomarkters
 def biomarkers():
+    '''
+        Biomarkers processing
+    '''
     BIOMARKERS_FILENAME = "factory/data/Biomarkers.txt"
     # read csv file
     bioData = pd.read_csv(BIOMARKERS_FILENAME, sep="\t", encoding='cp949')
@@ -16,8 +17,10 @@ def biomarkers():
     conditionfp2BetaData = fp2BetaData[condition]
     print(conditionfp2BetaData.mean())      # Average of Condition Fp2 Beta Data
 
-# Ssvep
 def ssvepProcess():
+    '''
+        Ssvep processing
+    '''
     # FP2_FILENAME = filename
     FP2_FILENAME = "factory/data/Fp2_FFT.txt"
     # read csv file
@@ -42,6 +45,136 @@ def ssvepProcess():
     #     time.sleep(3)
     #     print(dataSample)
 
+def p300Processing2(filePath):
+    ''' Main p300 processing '''
+    RAWDATA_FILENAME = filePath
+    # RAWDATA_FILENAME = 'factory/data/Rawdata.txt' # testing data Path
+
+    ''' Extract 'Time', 'EEG_Fp2' after read Rawdata.txt '''
+    rawData = pd.read_csv(RAWDATA_FILENAME, sep="\t", encoding='cp949')
+    extractData = rawData.loc[:, ['Time', 'EEG_Fp2']]
+
+    ''' 
+        Time의 성분중 min과 sec을 뽑아내서 min*60 + sec을 새로운 time으로 설정 
+        [-10:-8] = minute
+        [-7:] = sec
+    '''
+    extractData['Time'] = extractData['Time'].str[-10:-8].astype('float') * 60 + extractData['Time'].str[-7:].astype('float')
+    ''' 기준점이 되는 last data -> Rawdata에서 마지막 줄 데이터 '''
+    last = float(extractData['Time'].iloc[-1])
+    ''' last의 시간으로부터 6초전의 data 추출 '''
+    dataCondition = extractData['Time'].astype('float') > last - 6
+    extractData = extractData[dataCondition]
+
+    ''' 1.5초 전의 데이터 -> down '''
+    downCondition = extractData['Time'] < last - 1.5
+    downSide = extractData[downCondition]
+    down = downSide['EEG_Fp2'].mean()
+    print(downSide['EEG_Fp2'].mean())
+    ''' 3 ~ 1.5초 전의 데이터 -> up '''
+    upCondition = extractData['Time'].between(last - 3, last - 1.5)
+    upSide = extractData[upCondition]
+    up = upSide['EEG_Fp2'].mean()
+    print(upSide['EEG_Fp2'].mean())
+    ''' 4.5 ~ 3초 전의 데이터 -> right '''
+    rightCondition = extractData['Time'].between(last - 4.5, last - 3)
+    rightSide = extractData[rightCondition]
+    right = rightSide['EEG_Fp2'].mean()
+    print(rightSide['EEG_Fp2'].mean())
+    ''' 6 ~ 4.5초 전의 데이터 -> left '''
+    leftCondition = extractData['Time'].between(last - 6, last - 4.5)
+    leftSide = extractData[leftCondition]
+    left = leftSide['EEG_Fp2'].mean()
+    print(leftSide['EEG_Fp2'].mean())
+
+    ''' 비교결과 max에 해당하는 동작을 return '''
+    if max([left, right, up, down]) == left:
+        print("left")
+        return 'left'
+    elif max([left, right, up, down]) == right:
+        print("right")
+        return 'right'
+    elif max([left, right, up, down]) == up:
+        print("up")
+        return 'up'
+    elif max([left, right, up, down]) == down:
+        print("down")
+        return 'down'
+
+def fp2GraphImage(filePath, srcTime):
+    # BIOMARKERS_FILENAME = 'factory/data/Biomarkers.txt'   # tesing data Path
+    BIOMARKERS_FILENAME = filePath
+
+    ''' read Biomarkers.txt '''
+    bioData = pd.read_csv(BIOMARKERS_FILENAME, sep="\t", encoding='cp949')
+    ''' graph x position naming 
+        [21:23] = month
+        [24:26] = day
+        [30] = min
+        [32:34] = sec
+    '''
+    exeTime = BIOMARKERS_FILENAME[21:23] + '/' + BIOMARKERS_FILENAME[24:26] + ' ' + BIOMARKERS_FILENAME[30] + ':' + BIOMARKERS_FILENAME[32:34]
+
+    ''' extract Fp1_Beta, Fp2_Beta '''
+    extractFp1Beta = bioData['Fp1_Beta(%)']
+    extractFp2Beta = bioData['Fp2_Beta(%)']
+
+    ''' extract Fp1Beta > 15%, Fp2Beta > 15% '''
+    conditionFp1 = extractFp1Beta > 15
+    conditionFp2 = extractFp2Beta > 15
+
+    ''' each dataframe mean '''
+    avgTotalFp1 = extractFp1Beta.mean()
+    avgTotalFp2 = extractFp2Beta.mean()
+    avgConditionFp1 = extractFp1Beta[conditionFp1].mean()
+    avgConditionFp2 = extractFp2Beta[conditionFp2].mean()
+
+    ''' average Fp data & more than 15% data '''
+    avgTotal = (avgTotalFp1 + avgTotalFp2) / 2
+    avgCondition = (avgConditionFp1 + avgConditionFp2) / 2
+    print(avgTotal, avgCondition)
+
+    ''' 누적데이터 read '''
+    resultData = pd.read_csv('factory/data/result.txt', sep='\t')
+    ''' 누적데이터에 계산값 append '''
+    resultData = resultData.append({'Time': exeTime, 'Total': avgTotal, 'Concentration': avgCondition}, ignore_index=True)
+    ''' 누적데이터 다시 읽어서 마지막 5개의 데이터 extract '''
+    resultData.to_csv('factory/data/result.txt', sep='\t', index=False)
+    tailResult = resultData.tail()
+
+    ''' create x range '''
+    def create_x(t, w, n, d):
+        return [t * x + w * n for x in range(d)]
+
+    ''' data num condition '''
+    if len(tailResult) == 1:
+        d = 1
+    elif len(tailResult) == 2:
+        d = 2
+    elif len(tailResult) == 3:
+        d = 3
+    elif len(tailResult) == 4:
+        d = 4
+    elif len(tailResult) >= 5:
+        d = 5
+
+    ''' total, concentration x axis '''
+    X_TOTAL = create_x(2, 0.8, 1, d)
+    X_CONCEN = create_x(2, 0.8, 2, d)
+
+    ''' draw total, concentration bar '''
+    ax = plt.subplot()
+    ax.bar(X_TOTAL, tailResult['Total'])
+    ax.bar(X_CONCEN, tailResult['Concentration'])
+
+    ''' wrtie bar name '''
+    middle_x = [(a + b) / 2 for (a, b) in zip(X_TOTAL, X_CONCEN)]
+    ax.set_xticks(middle_x)
+    ax.set_xticklabels(tailResult['Time'])
+    # save graph img
+    plt.savefig('factory/image/result.png')
+
+    # plt.show()
 ''' -> not used 
 def p300Processing(filePath):
     RAWDATA_FILENAME = filePath
@@ -161,126 +294,6 @@ def p300Processing(filePath):
         print("down")
     #
 '''
-
-# main p300 processing
-def p300Processing2(filePath):
-    RAWDATA_FILENAME = filePath
-    # RAWDATA_FILENAME = 'factory/data/Rawdata.txt' # testing data Path
-
-    rawData = pd.read_csv(RAWDATA_FILENAME, sep="\t", encoding='cp949')     # read Rawdata.txt
-    extractData = rawData.loc[:, ['Time', 'EEG_Fp2']]   # extract 'Time', 'EEG_Fp2'
-
-    # Time의 성분중 min과 sec을 뽑아내서 min*60+sec을 새로운 time으로 설정
-    extractData['Time'] = extractData['Time'].str[-10:-8].astype('float') * 60 + extractData['Time'].str[-7:].astype('float')
-    # 기준점이 되는 last data -> Rawdata에서 마지막 줄 데이터
-    last = float(extractData['Time'].iloc[-1])
-    # last의 시간으로부터 6초전의 data 추출
-    dataCondition = extractData['Time'].astype('float') > last - 6
-    extractData = extractData[dataCondition]
-
-    # 1.5초 전의 데이터 -> down
-    downCondition = extractData['Time'] < last - 1.5
-    downSide = extractData[downCondition]
-    down = downSide['EEG_Fp2'].mean()
-    print(downSide['EEG_Fp2'].mean())
-    # 3 ~ 1.5초 전의 데이터 -> up
-    upCondition = extractData['Time'].between(last - 3, last - 1.5)
-    upSide = extractData[upCondition]
-    up = upSide['EEG_Fp2'].mean()
-    print(upSide['EEG_Fp2'].mean())
-    # 4.5 ~ 3초 전의 데이터 -> right
-    rightCondition = extractData['Time'].between(last - 4.5, last - 3)
-    rightSide = extractData[rightCondition]
-    right = rightSide['EEG_Fp2'].mean()
-    print(rightSide['EEG_Fp2'].mean())
-    # 6 ~ 4.5초 전의 데이터 -> left
-    leftCondition = extractData['Time'].between(last - 6, last - 4.5)
-    leftSide = extractData[leftCondition]
-    left = leftSide['EEG_Fp2'].mean()
-    print(leftSide['EEG_Fp2'].mean())
-
-    # 비교결과 max에 해당하는 position을 return
-    if max([left, right, up, down]) == left:
-        print("left")
-        return 'left'
-    elif max([left, right, up, down]) == right:
-        print("right")
-        return 'right'
-    elif max([left, right, up, down]) == up:
-        print("up")
-        return 'up'
-    elif max([left, right, up, down]) == down:
-        print("down")
-        return 'down'
-
-def fp2GraphImage(filePath):
-    # BIOMARKERS_FILENAME = 'factory/data/Biomarkers.txt'   # tesing data Path
-    BIOMARKERS_FILENAME = filePath
-
-    # read Biomarkers.txt
-    bioData = pd.read_csv(BIOMARKERS_FILENAME, sep="\t", encoding='cp949')
-    # graph x position naming
-    exeTime = bioData['Time'][0]
-
-    # extract Fp1_Beta, Fp2_Beta
-    extractFp1Beta = bioData['Fp1_Beta(%)']
-    extractFp2Beta = bioData['Fp2_Beta(%)']
-    # extract Fp1Beta > 15%, Fp2Beta > 15%
-    conditionFp1 = extractFp1Beta > 15
-    conditionFp2 = extractFp2Beta > 15
-
-    # each dataframe mean
-    avgTotalFp1 = extractFp1Beta.mean()
-    avgTotalFp2 = extractFp2Beta.mean()
-    avgConditionFp1 = extractFp1Beta[conditionFp1].mean()
-    avgConditionFp2 = extractFp2Beta[conditionFp2].mean()
-
-    # average Fp data & more than 15% data
-    avgTotal = (avgTotalFp1 + avgTotalFp2) / 2
-    avgCondition = (avgConditionFp1 + avgConditionFp2) / 2
-    print(avgTotal, avgCondition)
-
-    # 누적데이터 read
-    resultData = pd.read_csv('factory/data/result.txt', sep='\t')
-    # 누적데이터에 계산값 append
-    resultData = resultData.append({'Time': exeTime, 'Total': avgTotal, 'Concentration': avgCondition}, ignore_index=True)
-    # 누적데이터 다시 읽어서 마지막 5개의 데이터 extract
-    resultData.to_csv('factory/data/result.txt', sep='\t', index=False)
-    tailResult = resultData.tail()
-
-    # create x range
-    def create_x(t, w, n, d):
-        return [t * x + w * n for x in range(d)]
-
-    # data num condition
-    if len(tailResult) == 1:
-        d = 1
-    elif len(tailResult) == 2:
-        d = 2
-    elif len(tailResult) == 3:
-        d = 3
-    elif len(tailResult) == 4:
-        d = 4
-    elif len(tailResult) >= 5:
-        d = 5
-
-    # total, concentration x axis
-    X_TOTAL = create_x(2, 0.8, 1, d)
-    X_CONCEN = create_x(2, 0.8, 2, d)
-
-    # draw total, concentration bar
-    ax = plt.subplot()
-    ax.bar(X_TOTAL, tailResult['Total'])
-    ax.bar(X_CONCEN, tailResult['Concentration'])
-
-    # wrtie bar name
-    middle_x = [(a + b) / 2 for (a, b) in zip(X_TOTAL, X_CONCEN)]
-    ax.set_xticks(middle_x)
-    ax.set_xticklabels(tailResult['Time'])
-    # save graph img
-    plt.savefig('factory/image/result.png')
-
-    # plt.show()
 
 # testing
 # fp2GraphImage()
