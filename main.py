cc78d1e948226bdf01b625c13de65b8ab23e09bb
R _ R <exit19093@gmail.com>
2021-06-08 14:36:58 +0900

    update: commentart main

--- main.py
+++ main.py
@@ -11,48 +11,37 @@ from failPage import *
 class App(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
+        ''' Title '''
         self.title("Neuro")
-        # self.w = 1600
-        # self.h = 1200
-        # self.sw = self.winfo_screenwidth()
-        # self.sh = self.winfo_screenheight()
-        # self.x = (self.sw-self.w)/2
-        # self.y = (self.sh-self.h)/2
-
         self.geometry("1600x1200")
-        # self.geometry('%dx%d+%d+%d' % (self.w, self.h, self.x, self.y))
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
 
-        # main container
+        ''' Main container '''
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+        ''' Define frames '''
         self.frames = {}
 
+        ''' Page selection '''
         for F in (IntroPage, GraphPage, StartPage, Level1Page, Level2Page, Level3Page,
                   CompletePage, FailPage):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
-            # put all of the pages in the same location;
-            # the one on the top of the stacking order
-            # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="news")
 
+        ''' Initial Page '''
         self.show_frame("IntroPage")
 
     def show_frame(self, page_name, arg=None):
-        '''Show a frame for the given page name'''
+        ''' Show a frame for the given page name '''
         frame = self.frames[page_name]
         self.shared_data = arg
         frame.tkraise()
         frame.canvas.focus_set()
-        # print(self.shared_data)
-
-
-
 
 if __name__ == "__main__":
     app = App()
